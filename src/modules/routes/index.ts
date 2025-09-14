e245a902420079967b428f76a7bf27ddf9f3a29b
17Shanto <shahporanshanto23@gmail.com>
2025-09-14 09:23:45 +0600

    feat: Implement  Authentication functionalities

--- src/modules/routes/index.ts
+++ src/modules/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userRoute from "../user/user.route";
+import authRouter from "../auth/auth.route";
 
 const routes = Router();
 routes.use("/user", userRoute);
+routes.use("/auth", authRouter);
 
 export default routes;
